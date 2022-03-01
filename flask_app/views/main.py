5be8394bdb2bdf35b8a413080137708727f5ed06
ntorba <32570754+ntorba@users.noreply.github.com>
2022-03-01 15:49:54 -0500

    Update main.py

--- flask_app/views/main.py
+++ flask_app/views/main.py
@@ -2,18 +2,25 @@ from flask import render_template, request, redirect, Blueprint, url_for, flash
 
 from .forms import ClusterSelectionForm
 
-from ..build_feed import build_feed
+from ..build_feed import FeedDB, url_get_host
 
 main_blueprint = Blueprint("main", __name__, template_folder="templates")
 
+db = FeedDB("db/test_db2.json")
 
-@main_blueprint.route("/")
+
+@main_blueprint.route("/", methods=["GET", "POST"])
 def index():
     form = ClusterSelectionForm(request.form)
-    feed = build_feed("Python")
     if form.validate_on_submit():
-        return render_template("index.html", form=form, feed=feed)
-    return render_template("index.html", form=form, feed=feed)
+        external_url_feed = db.get_external_url_feed(form.data["cluster"])
+        return render_template(
+            "index.html", form=form, feed=external_url_feed, url_get_host=url_get_host
+        )
+    external_url_feed = db.get_external_url_feed("Ethereum")
+    return render_template(
+        "index.html", form=form, feed=external_url_feed, url_get_host=url_get_host
+    )
 
 
 @main_blueprint.route("/about/")
